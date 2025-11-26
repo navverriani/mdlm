c3989a897eb69175f8fb4bf57398eadc636da244
Davyd Naveriani <sandrodavid50@gmail.com>
2025-11-26 11:51:30 +0100

    rescoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import ast
 import gzip
 import json
 import fsspec
@@ -14,268 +15,257 @@ import dataloader
 import diffusion
 import utils
 
-omegaconf.OmegaConf.register_new_resolver(
-  'cwd', os.getcwd)
-omegaconf.OmegaConf.register_new_resolver(
-  'device_count', torch.cuda.device_count)
-omegaconf.OmegaConf.register_new_resolver(
-  'eval', eval)
-omegaconf.OmegaConf.register_new_resolver(
-  'div_up', lambda x, y: (x + y - 1) // y)
+omegaconf.OmegaConf.register_new_resolver("cwd", os.getcwd)
+omegaconf.OmegaConf.register_new_resolver("device_count", torch.cuda.device_count)
+omegaconf.OmegaConf.register_new_resolver("eval", eval)
+omegaconf.OmegaConf.register_new_resolver("div_up", lambda x, y: (x + y - 1) // y)
 
 
 def _load_from_checkpoint(config, tokenizer):
-  if 'hf' in config.backbone:
-    return diffusion.Diffusion(
-      config, tokenizer=tokenizer).to('cuda')
-  
-  return diffusion.Diffusion.load_from_checkpoint(
-    config.eval.checkpoint_path,
-    tokenizer=tokenizer,
-    config=config)
+    if "hf" in config.backbone:
+        return diffusion.Diffusion(config, tokenizer=tokenizer).to("cuda")
+
+    return diffusion.Diffusion.load_from_checkpoint(
+        config.eval.checkpoint_path, tokenizer=tokenizer, config=config
+    )
 
 
 @L.pytorch.utilities.rank_zero_only
 def _print_config(
-  config: omegaconf.DictConfig,
-  resolve: bool = True,
-  save_cfg: bool = True) -> None:
-  """Prints content of DictConfig using Rich library and its tree structure.
-  
-  Args:
-    config (DictConfig): Configuration composed by Hydra.
-    resolve (bool): Whether to resolve reference fields of DictConfig.
-    save_cfg (bool): Whether to save the configuration tree to a file.
-  """
+    config: omegaconf.DictConfig, resolve: bool = True, save_cfg: bool = True
+) -> None:
+    """Prints content of DictConfig using Rich library and its tree structure.
 
-  style = 'dim'
-  tree = rich.tree.Tree('CONFIG', style=style, guide_style=style)
+    Args:
+      config (DictConfig): Configuration composed by Hydra.
+      resolve (bool): Whether to resolve reference fields of DictConfig.
+      save_cfg (bool): Whether to save the configuration tree to a file.
+    """
 
-  fields = config.keys()
-  for field in fields:
-    branch = tree.add(field, style=style, guide_style=style)
+    style = "dim"
+    tree = rich.tree.Tree("CONFIG", style=style, guide_style=style)
 
-    config_section = config.get(field)
-    branch_content = str(config_section)
-    if isinstance(config_section, omegaconf.DictConfig):
-      branch_content = omegaconf.OmegaConf.to_yaml(
-        config_section, resolve=resolve)
+    fields = config.keys()
+    for field in fields:
+        branch = tree.add(field, style=style, guide_style=style)
 
-    branch.add(rich.syntax.Syntax(branch_content, 'yaml'))
-  rich.print(tree)
-  if save_cfg:
-    with fsspec.open(
-      '{}/config_tree.txt'.format(
-        config.checkpointing.save_dir), 'w') as fp:
-      rich.print(tree, file=fp)
+        config_section = config.get(field)
+        branch_content = str(config_section)
+        if isinstance(config_section, omegaconf.DictConfig):
+            branch_content = omegaconf.OmegaConf.to_yaml(
+                config_section, resolve=resolve
+            )
+
+        branch.add(rich.syntax.Syntax(branch_content, "yaml"))
+    rich.print(tree)
+    if save_cfg:
+        with fsspec.open(
+            "{}/config_tree.txt".format(config.checkpointing.save_dir), "w"
+        ) as fp:
+            rich.print(tree, file=fp)
 
 
 @L.pytorch.utilities.rank_zero_only
 def _print_batch(train_ds, valid_ds, tokenizer, k=64):
-  for dl_type, dl in [
-    ('train', train_ds), ('valid', valid_ds)]:
-    print(f'Printing {dl_type} dataloader batch.')
-    batch = next(iter(dl))
-    print('Batch input_ids.shape', batch['input_ids'].shape)
-    first = batch['input_ids'][0, :k]
-    last = batch['input_ids'][0, -k:]
-    print(f'First {k} tokens:', tokenizer.decode(first))
-    print('ids:', first)
-    print(f'Last {k} tokens:', tokenizer.decode(last))
-    print('ids:', last)
+    for dl_type, dl in [("train", train_ds), ("valid", valid_ds)]:
+        print(f"Printing {dl_type} dataloader batch.")
+        batch = next(iter(dl))
+        print("Batch input_ids.shape", batch["input_ids"].shape)
+        first = batch["input_ids"][0, :k]
+        last = batch["input_ids"][0, -k:]
+        print(f"First {k} tokens:", tokenizer.decode(first))
+        print("ids:", first)
+        print(f"Last {k} tokens:", tokenizer.decode(last))
+        print("ids:", last)
 
 
 def generate_samples(config, logger, tokenizer):
-  logger.info('Generating samples.')
-  model = _load_from_checkpoint(config=config,
-                                tokenizer=tokenizer)
-  model.gen_ppl_metric.reset()
-  if config.eval.disable_ema:
-    logger.info('Disabling EMA.')
-    model.ema = None
-  stride_length = config.sampling.stride_length
-  num_strides = config.sampling.num_strides
-  for _ in range(config.sampling.num_sample_batches):
-    if config.sampling.semi_ar:
-      _, intermediate_samples, _ = model.restore_model_and_semi_ar_sample(
-        stride_length=stride_length,
-        num_strides=num_strides,
-        dt=1 / config.sampling.steps)
-      text_samples = intermediate_samples[-1]
-      # Note: Samples generated using semi-ar method
-      # need to to be processed before computing generative perplexity
-      # since these samples contain numerous <|endoftext|> tokens
-      # and diffusion.compute_generative_perplexity() discards
-      # any text after the first EOS token.
-    else:
-      samples = model.restore_model_and_sample(
-        num_steps=config.sampling.steps)
-      text_samples = model.tokenizer.batch_decode(samples)
-      model.compute_generative_perplexity(text_samples)
-  print('Text samples:', text_samples)
-  if not config.sampling.semi_ar:
-    print('Generative perplexity:',
-          model.gen_ppl_metric.compute())
-  return text_samples
+    logger.info("Generating samples.")
+    model = _load_from_checkpoint(config=config, tokenizer=tokenizer)
+    model.gen_ppl_metric.reset()
+    if config.eval.disable_ema:
+        logger.info("Disabling EMA.")
+        model.ema = None
+    stride_length = config.sampling.stride_length
+    num_strides = config.sampling.num_strides
+    for _ in range(config.sampling.num_sample_batches):
+        if config.sampling.semi_ar:
+            _, intermediate_samples, _ = model.restore_model_and_semi_ar_sample(
+                stride_length=stride_length,
+                num_strides=num_strides,
+                dt=1 / config.sampling.steps,
+            )
+            text_samples = intermediate_samples[-1]
+            # Note: Samples generated using semi-ar method
+            # need to to be processed before computing generative perplexity
+            # since these samples contain numerous <|endoftext|> tokens
+            # and diffusion.compute_generative_perplexity() discards
+            # any text after the first EOS token.
+        else:
+            samples = model.restore_model_and_sample(num_steps=config.sampling.steps)
+            text_samples = model.tokenizer.batch_decode(samples)
+            model.compute_generative_perplexity(text_samples)
+    print("Text samples:", text_samples)
+    if not config.sampling.semi_ar:
+        print("Generative perplexity:", model.gen_ppl_metric.compute())
+    return text_samples
+
 
 def _ppl_eval(config, logger, tokenizer):
-  logger.info('Starting Zero Shot Eval.')
+    logger.info("Starting Zero Shot Eval.")
 
-  model = _load_from_checkpoint(config=config,
-                                tokenizer=tokenizer)
-  if config.eval.disable_ema:
-    logger.info('Disabling EMA.')
-    model.ema = None
+    model = _load_from_checkpoint(config=config, tokenizer=tokenizer)
+    if config.eval.disable_ema:
+        logger.info("Disabling EMA.")
+        model.ema = None
 
-  wandb_logger = None
-  if config.get('wandb', None) is not None:
-    wandb_logger = L.pytorch.loggers.WandbLogger(
-      config=omegaconf.OmegaConf.to_object(config),
-      ** config.wandb)
-  callbacks = []
-  if 'callbacks' in config:
-    for _, callback in config.callbacks.items():
-      callbacks.append(hydra.utils.instantiate(callback))
-  trainer = hydra.utils.instantiate(
-    config.trainer,
-    default_root_dir=os.getcwd(),
-    callbacks=callbacks,
-    strategy=hydra.utils.instantiate(config.strategy),
-    logger=wandb_logger)
-  _, valid_ds = dataloader.get_dataloaders(
-    config, tokenizer, skip_train=True, valid_seed=config.seed)
-  trainer.validate(model, valid_ds)
+    wandb_logger = None
+    if config.get("wandb", None) is not None:
+        wandb_logger = L.pytorch.loggers.WandbLogger(
+            config=omegaconf.OmegaConf.to_object(config), **config.wandb
+        )
+    callbacks = []
+    if "callbacks" in config:
+        for _, callback in config.callbacks.items():
+            callbacks.append(hydra.utils.instantiate(callback))
+    trainer = hydra.utils.instantiate(
+        config.trainer,
+        default_root_dir=os.getcwd(),
+        callbacks=callbacks,
+        strategy=hydra.utils.instantiate(config.strategy),
+        logger=wandb_logger,
+    )
+    _, valid_ds = dataloader.get_dataloaders(
+        config, tokenizer, skip_train=True, valid_seed=config.seed
+    )
+    trainer.validate(model, valid_ds)
 
 
 def _get_scores(config, logger, tokenizer):
-  logger.info('Starting Zero Shot Eval.')
+    import pprint
 
-  model = _load_from_checkpoint(config=config,
-                                tokenizer=tokenizer)
-  if config.eval.disable_ema:
-    logger.info('Disabling EMA.')
-    model.ema = None
+    model = _load_from_checkpoint(config=config, tokenizer=tokenizer)
+    if config.eval.disable_ema:
+        model.ema = None
 
-  batch_size = config.rescore.batch_size
+    batch_size = config.rescore.batch_size
 
-  wandb_logger = None
-  if config.get('wandb', None) is not None:
-    wandb_logger = L.pytorch.loggers.WandbLogger(
-      config=omegaconf.OmegaConf.to_object(config),
-      ** config.wandb)
-  callbacks = []
-  if 'callbacks' in config:
-    for _, callback in config.callbacks.items():
-      callbacks.append(hydra.utils.instantiate(callback))
+    wandb_logger = None
+    if config.get("wandb", None) is not None:
+        wandb_logger = L.pytorch.loggers.WandbLogger(
+            config=omegaconf.OmegaConf.to_object(config), **config.wandb
+        )
+    callbacks = []
+    if "callbacks" in config:
+        for _, callback in config.callbacks.items():
+            callbacks.append(hydra.utils.instantiate(callback))
 
-  model.eval()
-  if torch.cuda.is_available():
-    model = model.to("cuda")
+    model.eval()
+    if torch.cuda.is_available():
+        model = model.to("cuda")
 
-  lm_scores = {}
+    lm_scores = {}
 
-  if config.rescore.hypothesis_file.endswith(".gz"):
-    with gzip.open(config.rescore.hypotheses_file, 'rt') as f:
-      hypotheses_dict = json.load(f)
-  else:
-    with open(config.rescore.hypotheses_file, 'r') as f:
-      hypotheses_dict = json.load(f)
-
-  for utt_id, nbest_scores in hypotheses_dict.items():
-    hypotheses = [hyp for _, hyp in nbest_scores]
-    tokenized = tokenizer(
-      hypotheses,
-      return_tensors='pt',
-      padding=True,
-      truncation=True,
-      max_length=config.model.length
-    )
-
-    input_ids = tokenized['input_ids'].to('cuda')
-    attention_mask = tokenized['attention_mask'].to('cuda')
-
-    hyp_log_probs = []
-    with torch.no_grad():
-      for i in range(0, len(input_ids), batch_size):
-        batch_ids = input_ids[i:i + batch_size]
-        batch_mask = attention_mask[i:i + batch_size]
-
-        loss_output = model._loss(batch_ids, batch_mask)
-        batch_log_prob = -loss_output.nlls.sum(dim=-1)
-        hyp_log_probs.append(batch_log_prob)
-
-      log_probs = torch.cat(hyp_log_probs).cpu().tolist()
-      scored_hypotheses = list(zip(log_probs, hypotheses))
-
-    lm_scores[utt_id] = scored_hypotheses
-
-    if config.rescore.output_file.endswith('.gz'):
-      with gzip.open(config.rescore.output_file, 'wt') as f:
-        json.dump(lm_scores, f)
+    if config.rescore.hypothesis_file.endswith(".gz"):
+        with gzip.open(config.rescore.hypothesis_file, "rt") as f:
+            content = f.read()
+            hypotheses_dict = ast.literal_eval(content)
     else:
-      with open(config.rescore.output_file, 'w') as f:
-        json.dump(lm_scores, f)
+        with open(config.rescore.hypothesis_file, "r") as f:
+            content = f.read()
+            hypotheses_dict = ast.literal_eval(content)
 
-    return lm_scores
+    for utt_id, nbest_scores in hypotheses_dict.items():
+        hypotheses = [hyp for _, hyp in nbest_scores]
+        tokenized = tokenizer(
+            hypotheses,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=config.model.length,
+        )
 
+        input_ids = tokenized["input_ids"].to("cuda")
+        attention_mask = tokenized["attention_mask"].to("cuda")
 
+        hyp_log_probs = []
+        with torch.no_grad():
+            for i in range(0, len(input_ids), batch_size):
+                batch_ids = input_ids[i : i + batch_size]
+                batch_mask = attention_mask[i : i + batch_size]
+
+                loss_output = model._loss(batch_ids, batch_mask)
+                masked_nlls = loss_output.nlls * batch_mask
+                batch_log_prob = -masked_nlls.nlls.sum(dim=-1)
+                hyp_log_probs.append(batch_log_prob)
+
+            log_probs = torch.cat(hyp_log_probs).cpu().tolist()
+            scored_hypotheses = list(zip(log_probs, hypotheses))
+
+        lm_scores[utt_id] = scored_hypotheses
+
+    output_file = os.path.join(config.rescore.output_dir, "lm_scores.py.gz")
+
+    with gzip.open(output_file, "wt") as f:
+        f.write(pprint.pformat(lm_scores, width=120))
 
 
 def _train(config, logger, tokenizer):
-  logger.info('Starting Training.')
-  wandb_logger = None
-  if config.get('wandb', None) is not None:
-    wandb_logger = L.pytorch.loggers.WandbLogger(
-      config=omegaconf.OmegaConf.to_object(config),
-      ** config.wandb)
+    logger.info("Starting Training.")
+    wandb_logger = None
+    if config.get("wandb", None) is not None:
+        wandb_logger = L.pytorch.loggers.WandbLogger(
+            config=omegaconf.OmegaConf.to_object(config), **config.wandb
+        )
 
-  if (config.checkpointing.resume_from_ckpt
-      and config.checkpointing.resume_ckpt_path is not None
-      and utils.fsspec_exists(
-        config.checkpointing.resume_ckpt_path)):
-    ckpt_path = config.checkpointing.resume_ckpt_path
-  else:
-    ckpt_path = None
+    if (
+        config.checkpointing.resume_from_ckpt
+        and config.checkpointing.resume_ckpt_path is not None
+        and utils.fsspec_exists(config.checkpointing.resume_ckpt_path)
+    ):
+        ckpt_path = config.checkpointing.resume_ckpt_path
+    else:
+        ckpt_path = None
 
-  # Lightning callbacks
-  callbacks = []
-  if 'callbacks' in config:
-    for _, callback in config.callbacks.items():
-      callbacks.append(hydra.utils.instantiate(callback))
+    # Lightning callbacks
+    callbacks = []
+    if "callbacks" in config:
+        for _, callback in config.callbacks.items():
+            callbacks.append(hydra.utils.instantiate(callback))
 
-  train_ds, valid_ds = dataloader.get_dataloaders(
-    config, tokenizer)
-  _print_batch(train_ds, valid_ds, tokenizer)
+    train_ds, valid_ds = dataloader.get_dataloaders(config, tokenizer)
+    _print_batch(train_ds, valid_ds, tokenizer)
 
-  model = diffusion.Diffusion(
-    config, tokenizer=valid_ds.tokenizer)
+    model = diffusion.Diffusion(config, tokenizer=valid_ds.tokenizer)
 
-  trainer = hydra.utils.instantiate(
-    config.trainer,
-    default_root_dir=os.getcwd(),
-    callbacks=callbacks,
-    strategy=hydra.utils.instantiate(config.strategy),
-    logger=wandb_logger)
-  trainer.fit(model, train_ds, valid_ds, ckpt_path=ckpt_path)
+    trainer = hydra.utils.instantiate(
+        config.trainer,
+        default_root_dir=os.getcwd(),
+        callbacks=callbacks,
+        strategy=hydra.utils.instantiate(config.strategy),
+        logger=wandb_logger,
+    )
+    trainer.fit(model, train_ds, valid_ds, ckpt_path=ckpt_path)
 
 
-@hydra.main(version_base=None, config_path='configs',
-            config_name='config')
+@hydra.main(version_base=None, config_path="configs", config_name="config")
 def main(config):
-  """Main entry point for training."""
-  L.seed_everything(config.seed)
-  _print_config(config, resolve=True, save_cfg=True)
-  
-  logger = utils.get_logger(__name__)
-  tokenizer = dataloader.get_tokenizer(config)
+    """Main entry point for training."""
+    L.seed_everything(config.seed)
+    _print_config(config, resolve=True, save_cfg=True)
 
-  if config.mode == 'sample_eval':
-    generate_samples(config, logger, tokenizer)
-  elif config.mode == 'ppl_eval':
-    _ppl_eval(config, logger, tokenizer)
-  else:
-    _train(config, logger, tokenizer)
+    logger = utils.get_logger(__name__)
+    tokenizer = dataloader.get_tokenizer(config)
+
+    if config.mode == "sample_eval":
+        generate_samples(config, logger, tokenizer)
+    elif config.mode == "ppl_eval":
+        _ppl_eval(config, logger, tokenizer)
+    elif config.mode == "rescore":
+        _get_scores(config, logger, tokenizer)
+    else:
+        _train(config, logger, tokenizer)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
